5fa5342781fcecd955e91f5ef156c78d227e08eb
yvanwang1992 <yffswyf@live.com>
2016-05-14 18:42:18 +0800

    Reorganise the code and add some notes.

--- YFRollingLabel.m
+++ YFRollingLabel.m
@@ -48,18 +48,20 @@
     self.isStopWhenTouch = YES;
     self.clipsToBounds = YES;
     
+    //Get all the auto layout Size of arrayText;
     CGSize maxSize = CGSizeMake(CGFLOAT_MAX, KSelfHeight);
-
     for (int i = 0; i < _textArray.count; i++) {
         CGRect textRect = [((NSString *)_textArray[i]) boundingRectWithSize:maxSize options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:(_font ? _font : kFont)} context:nil];
         [_textRectArray addObject:[NSValue valueWithCGRect:textRect]];
-    }
+    } 
     
+    //Add Subviews
     if(_textArray.count == 1){
         CGRect rect = [((NSValue *)[_textRectArray firstObject]) CGRectValue];
         if(rect.size.width > KSelfWith){
             _CanRolling = YES;
         }else{
+            //Add Another Single UILabel which won't roll.
             _CanRolling = NO;
             UILabel *label = [[UILabel alloc] initWithFrame:
                               CGRectMake((KSelfWith - rect.size.width) / 2, (KSelfHeight - rect.size.height) / 2, rect.size.width, rect.size.height)];
@@ -69,17 +71,16 @@
             label.userInteractionEnabled = YES;
             label.textColor = _textColor ? _textColor : [UIColor blackColor];
             label.font = _font ? _font : kFont;
+            label.tag = 100 + 2;
             [self addSubview:label];
             
-            label.tag = 100 + 2;
             UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(labelTap:)];
-            
             [label addGestureRecognizer:tapGesture];
         }
     }else{  //>1
         _CanRolling = YES;
     }
-    
+    //Add Two UILabels which can roll
     if(_CanRolling){
         self.orientation = RollingOrientationLeft;
         for (int i = 0; i < kNumberOfLabel; i++) {
@@ -90,17 +91,16 @@
             _labels[i].userInteractionEnabled = YES;
             _labels[i].textColor = _textColor ? _textColor : [UIColor whiteColor];
             _labels[i].font = _font ? _font : kFont;
-
-            UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(labelTap:)];
-            
-            [_labels[i] addGestureRecognizer:tapGesture];
-            
             [self addSubview:_labels[i]];
+            
+            UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(labelTap:)];
+            [_labels[i] addGestureRecognizer:tapGesture];
         }
         [self startTimer];
     }
 }
 
+//UILabel Tap Event
 -(void)labelTap:(UITapGestureRecognizer *)gesture{
     if(self.isStopWhenTouch){
         NSInteger tag = ((UILabel *)[gesture view]).tag - 100;
@@ -109,14 +109,14 @@
             index = _currentIndex;
         }else if (tag == 1){  //The Next One
             index = (_currentIndex + 1) % _textArray.count;
-        }else{  //One Label
+        }else{  //Only One Label
             index = _currentIndex;
         }
+        
         //Label Click Block
         if(self.labelClickBlock){
             self.labelClickBlock(index);
         }
-        
     }
 }
 
@@ -125,17 +125,19 @@
 }
 
 #pragma Relation Methods of _timer
-
-
 -(void)beginTimer{
     if(_timer == nil){
         [self startTimer];
         return;
     }
-    
     if(_timer && [_timer isValid] && _CanRolling){
         [_timer setFireDate:[NSDate date]];
     }
+}
+
+-(void)startTimer{
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.02 target:self selector:@selector(timerAction:) userInfo:nil repeats:YES];
+    [[NSRunLoop mainRunLoop] addTimer:_timer forMode:NSRunLoopCommonModes];
 }
 
 -(void)pauseTimer{
@@ -149,55 +151,86 @@
     _timer = nil;
 }
 
-
--(void)startTimer{
-    _timer = [NSTimer scheduledTimerWithTimeInterval:0.02 target:self selector:@selector(timerAction:) userInfo:nil repeats:YES];
-    [[NSRunLoop mainRunLoop] addTimer:_timer forMode:NSRunLoopCommonModes];
-}
-
-
-
 -(void)timerAction:(NSTimer *)timer{
 
-    NSArray *labelArray = [self GetLabelRectAtIndex:_currentIndex];
+    NSArray *labelArray = [self GetLabelRectArrayAtIndex:_currentIndex];//get reseted labelArray
     CGRect firstRect = [((NSValue *)[labelArray firstObject]) CGRectValue];
     CGRect lastRect = [((NSValue *)[labelArray lastObject]) CGRectValue];
     
-    CGFloat off;    // Next Label's OffsetX.
-    NSInteger sign; //From Left To Right or From Right To Left.
+    NSInteger sign; //sign for From Left To Right or From Right To Left.
     sign = (self.orientation == RollingOrientationLeft) ? 1 : -1;
-    self.offsetX =  self.offsetX - sign * self.speed;//从0 开始    前一个位置
+    self.offsetX =  self.offsetX - sign * self.speed;   //frame.origin.X of first Rect
     
-    //这个有可能有些问题
-    off = self.offsetX + sign * (((self.orientation == RollingOrientationLeft)? firstRect.size.width : lastRect.size.width) + self.internalWidth);   //后一个位置
-    //
-    NSArray *labelTextArray = [self GetLabelTextAtIndex:_currentIndex];
-        //当前全部在SelfWidth左边的时候
+    // Next Label's OffsetX:
+    // There is always internalWith's distance between Two Labels in Width.
+    // Like : [firstLabel][internalWidth][lastLabel] or Left-side right
+    CGFloat nextOffX = self.offsetX + sign * (((self.orientation == RollingOrientationLeft)? firstRect.size.width : lastRect.size.width) + self.internalWidth);   //frame.origin.X of last Rect
     
-        //获取现在后一个的实际位置   赋给_offsetX
-
-        //全部移动到最左边后   左边不显示  右边
-        if((self.offsetX > -firstRect.size.width && self.orientation == RollingOrientationLeft) ||
+    
+    //get reseted labelTextArray
+    NSArray *labelTextArray = [self GetLabelTextArrayAtIndex:_currentIndex];
+    
+    //        from Right to Left
+    //  <===============================
+    //firstLabel is always on the left.
+    //if the offsetX is bigger than Width of the firstLabel , offsetX and nextOffX
+    //      are both divided by speed for every _timer's Intervel(0.02);
+    //otherwise when firstLabel is invisible, Set FirstLabel's offsetX with LastLabel's
+    //      and change next two Labels whitch will be visible.
+    
+    //         from Left to Right
+    //  ===============================>
+    //firstLabel is always on the right.
+    //if the offsetX is bigger than Width of the View,        offsetX and nextOffX
+    //      are both added by speed for every _timer's Intervel(0.02);
+    //otherwise when firstLabel is invisible, Set FirstLabel's offsetX with LastLabel's
+    //      and change next two Labels whitch will be visible.
+    
+    //So The next time , Two Labels is exchanged;
+    //Set Two Label's Background and you'll observe it clearly;
+    
+    if((self.offsetX > -firstRect.size.width && self.orientation == RollingOrientationLeft) ||
            (self.offsetX < KSelfWith && self.orientation == RollingOrientationRight) ){
         
-            _labels[0].frame = CGRectMake(self.offsetX, (KSelfHeight-firstRect.size.height)/2, firstRect.size.width, firstRect.size.height);
-            _labels[0].text = [labelTextArray firstObject];
+        _labels[0].frame = CGRectMake(self.offsetX, (KSelfHeight-firstRect.size.height)/2, firstRect.size.width, firstRect.size.height);
+        _labels[0].text = [labelTextArray firstObject];
             
-            _labels[1].frame = CGRectMake(off, (KSelfHeight-lastRect.size.height) / 2, lastRect.size.width, lastRect.size.height);
-            _labels[1].text = [labelTextArray lastObject];
-        }
-        else if((self.offsetX <= -firstRect.size.width  && self.orientation == RollingOrientationLeft) ||
+        _labels[1].frame = CGRectMake(nextOffX, (KSelfHeight-lastRect.size.height) / 2, lastRect.size.width, lastRect.size.height);
+        _labels[1].text = [labelTextArray lastObject];
+    }
+    else if((self.offsetX <= -firstRect.size.width  && self.orientation == RollingOrientationLeft) ||
                 (self.offsetX >= KSelfWith && self.orientation == RollingOrientationRight)){
-            self.offsetX = _labels[1].frame.origin.x;
+        self.offsetX = _labels[1].frame.origin.x;
             
-            _currentIndex = (_currentIndex + 1)  % _textArray.count;
-        }
+        _currentIndex = (_currentIndex + 1)  % _textArray.count;
+    }
 }
 
 
+-(NSArray *)GetLabelTextArrayAtIndex:(NSInteger)index{
+    NSMutableArray *labelTextArray = [NSMutableArray arrayWithCapacity:kNumberOfLabel];
+    [labelTextArray removeAllObjects];
+    [labelTextArray addObject:[_textArray objectAtIndex:index]];
+    [labelTextArray addObject:[_textArray objectAtIndex:(index + 1) % _textArray.count]];
+    return labelTextArray;
+}
+
+-(NSArray *)GetLabelRectArrayAtIndex:(NSInteger)index{
+    NSMutableArray *labelRectArray = [NSMutableArray arrayWithCapacity:kNumberOfLabel];
+    [labelRectArray removeAllObjects];
+    NSValue *firstValue = [_textRectArray objectAtIndex:index];
+    NSValue *lastValue = [_textRectArray objectAtIndex:(index + 1) % _textArray.count];
+    [labelRectArray addObject:firstValue];
+    [labelRectArray addObject:lastValue];
+    return labelRectArray;
+}
+
+
+
+#pragma setter and getter
 //the speed should be setted bigger than 0 and no smaller than 5;
 -(void)setSpeed:(CGFloat)speed{
-    if(speed > 0 && speed <= 5){
+    if(speed > 0){
         _speed = speed;
     }
 }
@@ -206,30 +239,6 @@
     if(internalWidth > 0){
         _internalWidth = internalWidth;
     }
-}
-
--(void)setOrientation:(RollingOrientation)orientation{
-    _orientation = orientation;
-    self.offsetX = 0;
-    _currentIndex = 0;
-}
-
--(NSArray *)GetLabelTextAtIndex:(NSInteger)index{
-    NSMutableArray *labelTextArray = [NSMutableArray arrayWithCapacity:kNumberOfLabel];
-    [labelTextArray removeAllObjects];
-    [labelTextArray addObject:[_textArray objectAtIndex:index]];
-    [labelTextArray addObject:[_textArray objectAtIndex:(index + 1) % _textArray.count]];
-    return labelTextArray;
-}
-
--(NSArray *)GetLabelRectAtIndex:(NSInteger)index{
-    NSMutableArray *labelRectArray = [NSMutableArray arrayWithCapacity:kNumberOfLabel];
-    [labelRectArray removeAllObjects];
-    NSValue *firstValue = [_textRectArray objectAtIndex:index];
-    NSValue *lastValue = [_textRectArray objectAtIndex:(index + 1) % _textArray.count];
-    [labelRectArray addObject:firstValue];
-    [labelRectArray addObject:lastValue];
-    return labelRectArray;
 }
 
 -(BOOL)isCanRolling{
--- YFRollingLabel/YFViewController.m
+++ YFRollingLabel/YFViewController.m
@@ -18,17 +18,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
+    
+    //Add ScrollView
     UIScrollView *scrollView = [[UIScrollView  alloc] initWithFrame:self.view.bounds];
     scrollView.backgroundColor = [UIColor grayColor];
-    scrollView.contentSize = CGSizeMake(300, 1000);
+    scrollView.contentSize = CGSizeMake(self.view.frame.size.width, 1000);
     [self.view addSubview:scrollView];
-    NSArray *textArray = //@[@"Windows Phone，力图打破人们与信息和应用之间的隔阂，提供适用于人们包括工作和娱乐在内完整生活的方方面面，最优秀的端到端体验。"];
-    //  @[@"健康游戏忠告:",@"抵制不良游戏 拒绝盗版游戏",@"注意自我保护 谨防受骗上当",@"适度游戏益脑 沉迷游戏伤身",@"合理安排时间 享受健康生活"];
-//    @[@"This is the first text",@"This is the second text",@"This is the third text",@"This is the fouth text",@"This is the fifth text"];
     
-    @[@"THERE IS ONLY ONE TEXT IN THIS VIEW......"];
+    //Add YFRollingLabel
+    NSArray *textArray = @[@"THIS IS THE FIRST TEXT",
+                           @"THIS IS THE SECOND TEXT",
+                           @"THIS IS THE THIRD TEXT",
+                           @"THIS IS THE FOURTH TEXT",
+                           @"THIS IS THE FIFTH TEXT"];
+    //@[@"THERE IS ONLY ONE TEXT IN THIS VIEW......"];
+    
     
     _label = [[YFRollingLabel alloc] initWithFrame:CGRectMake(10, 100, self.view.frame.size.width - 20, 40)  textArray:textArray font:[UIFont systemFontOfSize:20] textColor:[UIColor whiteColor]];
     [scrollView addSubview:_label];
@@ -37,34 +42,29 @@
     [_label setOrientation:RollingOrientationLeft];
     [_label setInternalWidth:_label.frame.size.width / 3];;
     
-    
-    __weak typeof(self) weakSelf = self;
+    //Label Click Event Using Block
     _label.labelClickBlock = ^(NSInteger index){
-        NSString *result = [textArray objectAtIndex:index];
-        
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:[NSString stringWithFormat:@"第%li项", (long)index] message:result preferredStyle:UIAlertControllerStyleAlert];
-        [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:^(UIAlertAction * action) {
-            [weakSelf dismissViewControllerAnimated:YES completion:nil];
-        }]];
-        
-        [weakSelf presentViewController:alert animated:YES completion:nil];
-};
+        NSString *text = [textArray objectAtIndex:index];
+        NSLog(@"You Tapped item:%li , and the text is %@",(long)index,text);
+    };
 
     
-    NSArray *array = @[@"开始",@"暂停",@"停止", @"慢速"];
+    //Timer Control & Property Changed
+    NSArray *array = @[@"Timer Start",@"Timer Pause",@"Timer Stop",
+                       @"Set Orientation:Left/Right",@"Set Slow Speed(0.1)",@"Set InternalWidth"];
     for (int i = 0; i < array.count ; i++) {
-        CGFloat width = (self.view.frame.size.width) / array.count;
-        UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(i * width + 10, 350, width - 20, 50)];
+        UIButton *btn = [[UIButton alloc] initWithFrame:
+                         CGRectMake(50,CGRectGetMaxY(_label.frame) + i * 50 + 20, self.view.frame.size.width - 100, 40)];
         [btn setTitle:array[i] forState:UIControlStateNormal];
         [btn setBackgroundColor:[UIColor blueColor]];
         btn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
-        [btn addTarget:self action:@selector(btnAction:) forControlEvents:UIControlEventTouchUpInside];
+        [btn addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
         btn.tag = 100 + i;
         [scrollView addSubview:btn];
     }
 }
 
--(void)btnAction:(UIButton *)btn{
+-(void)buttonAction:(UIButton *)btn{
     NSInteger tag = btn.tag - 100;
     switch (tag) {
         case 0:
@@ -76,11 +76,25 @@
         case 2:
             [_label stopTimer];
             break;
+            
+            
         case 3:
-            _label.speed = 1;
+            if(_label.orientation == RollingOrientationLeft){
+                [_label setOrientation:RollingOrientationRight];
+            }else{
+                [_label setOrientation:RollingOrientationLeft];
+            }
+            break;
+        case 4:
+            [_label setSpeed:0.1];
+            break;
+        case 5:
+            [_label setInternalWidth:_label.frame.size.width / 2];
             break;
     }
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
